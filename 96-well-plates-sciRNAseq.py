7a7b30768f362ef29261f39177059278df520e7f
Alexander Chamessian <achamess@gmail.com>
2018-01-01 11:42:10 -0500

    fixed indents

--- 96-well-plates-sciRNAseq.py
+++ 96-well-plates-sciRNAseq.py
@@ -5,15 +5,15 @@ Given two primer sets (P5 and P7), makes combination of primers.
 # Make a series by column (A1,B1,C1...)
 # Add whatever suffix you want to
 
- for num in range (1,8):
- 	for let in range(65,73):
- 		print(chr(let)+str(num)+"_P7")
+for num in range (1,8):
+     for let in range(65,73):
+         print(chr(let)+str(num)+"_P7")
 
 
 # Make a plate by row (A1,A2,A3)
 
 for let in range(65,73):
-	for num in range (1,13):
-		print(chr(let)+str(num)+"_P5")
+    for num in range (1,13):
+        print(chr(let)+str(num)+"_P5")
 
                 
