91c6bd6314981e7fb248af540f8ff187bb735931
Alexander Chamessian <achamess@gmail.com>
2018-01-01 11:28:52 -0500

    initial commit

---
+++ 96-well-plates-sciRNAseq.py
@@ -0,0 +1,17 @@
+
+
+# Make a series by column (A1,B1,C1...)
+# Add whatever suffix you want to
+
+ for num in range (1,8):
+        for let in range(65,73):
+                print(chr(let)+str(num)+"_P7")
+
+
+# Make a plate by row (A1,A2,A3)
+
+for let in range(65,73):
+     for num in range (1,13):
+            print(chr(let)+str(num)+"_P5")
+
+                
